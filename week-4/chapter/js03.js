556065a5d4d0519b67859ce24910eb64868a1cbb
S-Skaggs <s-skaggs@outlook.com>
2024-01-31 20:10:29 -0500

    FEAT: Chapter work in progress.

---
+++ week-4/chapter/js03.js
@@ -0,0 +1,14 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Sheldon Skaggs
+     Date:   1/31/2024
+
+     Filename: js03.js
+ */
+
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
